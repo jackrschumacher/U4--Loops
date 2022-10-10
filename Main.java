6b7e2ca5823939f97918186b43b7287c5f206e95
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-10 16:38:31 +0000

    Planning

--- Main.java
+++ Main.java
@@ -10,16 +10,14 @@ class Main {
     int number =4;
 
     //do while loop (we use this when we always want the loop to run at least one time)
-// hello
-    
+
     do{
       number = input.nextInt();
       if(number < 2){
         System.out.println("Please choose a number 2 or greater.");
       }
     }while(number < 2);
-  
-    
-    
+
+  in 
   }
 }
